80e326050f1b16069b134ad380040539464a8694
ElGhostWolf <Hmjuan2108@gmail.com>
2024-09-21 12:31:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const mensajeInicial = document.getElementById('mensaje-inicial');
 const botonMostrar = document.getElementById('mostrar-imagenes');
 const imageContainer = document.querySelector('.image-container');
 const images = document.querySelectorAll('.image');
-const descripcion = document.querySelector('.image-description');
+const descripcion = document.getElementById('descripcion');
 
 botonMostrar.addEventListener('click', () => {
-    mensajeInicial.classList.add('hidden');
-    botonMostrar.classList.add('hidden');
-    imageContainer.classList.remove('hidden');
-    descripcion.classList.remove('hidden');
+    mensajeInicial.style.display = 'none';
+    imageContainer.style.display = 'flex';
+    descripcion.style.display = 'block';
+    // Mostrar la imagen
+    images[0].style.display = 'block';
 });
